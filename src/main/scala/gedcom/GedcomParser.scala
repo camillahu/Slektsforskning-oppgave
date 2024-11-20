c43c76f273f7900e220a2f2e4803cc87ec09701e
camillahu <camillahaugenurdal@gmail.com>
2024-11-20 14:47:03 +0100

    finished task

--- src/main/scala/gedcom/GedcomParser.scala
+++ src/main/scala/gedcom/GedcomParser.scala
@@ -18,7 +18,9 @@ object GedcomParser {
                              birthDate: Option[String] = None,
                              birthPlace: Option[String] = None,
                              deathDate: Option[String] = None,
-                             deathPlace: Option[String] = None
+                             deathPlace: Option[String] = None,
+                             hasBirthEvent: Boolean = false,
+                             hasDeathEvent: Boolean = false
                             )
 
 
@@ -37,15 +39,28 @@ object GedcomParser {
       line match {
         case l if l.startsWith("0 @") && l.endsWith("INDI") => builder.copy(id = l.split("@")(1))
         case l if l.startsWith("1 NAME") => builder.copy(name = parseName(l.substring(7)))
-        case l if l.startsWith("1 BIRT") => builder // Birth event found, no change
-        case l if l.startsWith("2 DATE") && builder.birthDate.isEmpty && l.length > 7 =>
+
+        case l if l.startsWith("1 BIRT") => builder.copy(hasBirthEvent = true)
+        case l if l.startsWith("2 DATE")
+          && builder.hasBirthEvent
+          && builder.birthDate.isEmpty
+          && l.length > 7 =>
           builder.copy(birthDate = Some(l.substring(7)))
-        case l if l.startsWith("2 PLAC") && builder.birthPlace.isEmpty && l.length > 7  =>
+        case l if l.startsWith("2 PLAC")
+          && builder.hasBirthEvent
+          && builder.birthDate.isEmpty
+          && l.length > 7 =>
           builder.copy(birthPlace = Some(l.substring(7).replaceAll(regexComma, "").trim))
-        case l if l.startsWith("1 DEAT") => builder // Death event found, no change
-        case l if l.startsWith("2 DATE") && builder.deathDate.isEmpty && l.length > 7  =>
+        case l if l.startsWith("1 DEAT") => builder.copy(hasBirthEvent = true)
+        case l if l.startsWith("2 DATE")
+          && builder.hasDeathEvent
+          && builder.deathDate.isEmpty
+          && l.length > 7  =>
           builder.copy(deathDate = Some(l.substring(7)))
-        case l if l.startsWith("2 PLAC") && builder.deathPlace.isEmpty && l.length > 7  =>
+        case l if l.startsWith("2 PLAC")
+          && builder.hasDeathEvent
+          && builder.deathPlace.isEmpty
+          && l.length > 7  =>
           builder.copy(deathPlace = Some(l.substring(7).replaceAll(regexComma, "").trim))
         case _ => builder
       }
